ff1cc9c36fc603c02f0b9da889648919d80fe8d3
jlwanjia <jlwanjia@gmail.com>
2022-02-21 18:40:10 -0800

    Initial commit

---
+++ MSSQLCSharp/dbo.Judo.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [MemberID]  INT          NOT NULL,
+    [FirstName] VARCHAR (50) NOT NULL,
+    [LastName]  VARCHAR (50) NOT NULL,
+    [DOB]       VARCHAR (50) NOT NULL,
+    [BeltRank]  VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([MemberID] ASC)
+);
+
